21e759f8a098e9958f4d388527d11f791eb1a624
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-12-05 22:25:06 +0530

    initial project

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//middleware
+app.use(express.json()); //this will allow us to send data in json format
+app.use(cors()); //this will allow us to send data from frontend to backend
+
+app.listen(5000, () => {
+  console.log("SERVER STARTED");
+});
