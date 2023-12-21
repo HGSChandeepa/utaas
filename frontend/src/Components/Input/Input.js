0e700fbddaa43e7d9f1cf3a350ecdb17c25f52f4
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2023-12-21 09:23:08 +0530

    Add upadate codes

---
+++ frontend/src/Components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Input = ({ label, type, placeholder }) => {
+  return (
+    <div className="flex flex-col lg:flex-col mx-auto items-start justify-around lg:mb-0">
+      <label className="mb-2 ml-2 lg:ml-4 ">{label}</label>
+      <input
+       className="border rounded-full py-2 px-3 mb-4 text-grey-darker w-72"
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,45 +3,43 @@ import Logo from "../Components/Logo/Logo";
 import Button from "../Components/Button/button";
 import Image3 from "../assets/image3.svg";
 import { useNavigate } from "react-router-dom";
+import Input from "../Components/Input/Input";
 
 const Register = (props) => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center">
-      <div className="bg-white flex flex-col justify-normal items-center rounded px-auto mx-auto lg:items-center w-[900px] ">
+      <div className="bg-white flex flex-col justify-normal items-center rounded px-4 lg:px-8 py-4 lg:pt-6 lg:pb-8 mx-auto lg:items-center lg:w-[900px]">
         <div className="align-top items-top">
           <Logo />
         </div>
-        <div className="text-center lg:mx-auto ">
+        <div className="text-center lg:mx-auto">
           <div>
-            <h1 className="text-[#4743E0] text-2xl lg:text-6xl font-extrabold mb-2 ">
+            <h1 className="text-[#4743E0] text-2xl lg:text-6xl font-extrabold mb-2">
               Input Your Information
             </h1>
-            <p className="mx-auto my-auto text-opacity-50 lg:w-1/2 opacity-40 text-center mb-2">
+            <p className="mx-auto my-auto text-opacity-50 lg:w-3/4 opacity-40 text-center mb-2">
               We need you to help us with some basic information for your
-              account creation. Here are our terms and conditiOnS. Please read
-              them carefully. We are GDRP compliant.
+              account creation. Here are our terms and conditions. Please read
+              them carefully. We are GDPR compliant.
             </p>
           </div>
-          <hr className="mx-auto  border-dashed rounded-md w-[800px]" />
-          <div className="flex flex-col  lg:flex-row justify-around py-2 my-5">
-            <form className="flex flex-col lg:flex-col mx-auto items-start justify-around ">
-              <label className="mb-2 ml-2 lg:ml-4 ">First Name</label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="text"
-                placeholder="john"
+
+          <hr className="mx-auto border-dashed rounded-md w-[80%] lg:w-[800px]" />
+          <div className="flex flex-col lg:flex-row justify-around py-2 my-5">
+            <form className="flex flex-col lg:flex-col mx-auto items-start justify-around mb-4 lg:mb-0">
+              <Input label={"First Name"} type={"text"} placeholder={"john"} />
+              <Input
+                label={"Work Email"}
+                type={"text"}
+                placeholder={"johnsmith@gmail.com"}
               />
 
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">Work Email</label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="text"
-                placeholder="johnsmith@gmail.com"
-              />
-
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">Department</label>
-              <select className="border rounded-full py-2 px-3 text-grey-darker w-72">
+              <label className="mb-2 ml-2 lg:ml-4">Department</label>
+              <select
+                className="border rounded-full py-2 px-3 mb-4 text-grey-darker w-72"
+                defaultValue=""
+              >
                 <option value="" disabled>
                   Select Deparment
                 </option>
@@ -50,35 +48,25 @@ const Register = (props) => {
                 <option value="">MME</option>
                 <option value="">IS</option>
               </select>
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">Password</label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="password"
-                placeholder="********"
+
+              <Input
+                label={"Password"}
+                type={"password"}
+                placeholder={"********"}
               />
             </form>
 
-            <form className="flex flex-col lg:flex-col mx-auto px-2 items-start justify-around">
-              <label className="mb-2 ml-2 lg:ml-4">Last Name</label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="text"
-                placeholder="smith"
+            <form className="flex flex-col lg:flex-col mx-auto items-start justify-around py-">
+              <Input label={"Last Name"} type={"text"} placeholder={"smith"} />
+              <Input
+                label={"Telephone Number"}
+                type={"tel"}
+                placeholder={"+94 000 000 000"}
               />
 
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">
-                Telephone Number
-              </label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="tel"
-                pattern="[+]{1}[0-9\s]{1,}"
-                placeholder="+94 000 000 000"
-              />
-
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">Role</label>
+              <label className="mb-2 ml-2 lg:ml-4">Role</label>
               <select
-                className="border rounded-full py-2 px-3 text-grey-darker w-72"
+                className="border rounded-full py-2 px-3 mb-4 text-grey-darker w-72"
                 defaultValue=""
               >
                 <option value="" disabled>
@@ -87,21 +75,17 @@ const Register = (props) => {
                 <option value="Head of Deparment">HoD</option>
                 <option value="user">Lecturer</option>
               </select>
-              
-              <label className="mb-2 ml-2 lg:ml-4 pt-2 mt-10">
-                Conform Password
-              </label>
-              <input
-                className="border rounded-full py-2 px-3 text-grey-darker w-72 "
-                type="password"
-                placeholder="********"
+              <Input
+                label={"Conform Password"}
+                type={"password"}
+                placeholder={"********"}
               />
             </form>
           </div>
         </div>
-        <div className="mt-4 lg:justify-between items-center">
-          <hr className="mx-auto  border-dashed rounded-md w-[800px]" />
-          <div className="flex flex-row lg:justify-between items-center mt-4">
+        <div className="mt-4 lg:justify-between items-center w-full lg:w-[80%]">
+        <hr className="mx-auto border-dashed rounded-md w-[80%] lg:w-[800px]" />
+          <div className="flex flex-col lg:flex-row justify-between items-center mt-4 ">
             <div className="flex flex-row ">
               <h1 className="mr-2">Already a member?</h1>
               <button
@@ -121,8 +105,8 @@ const Register = (props) => {
         </div>
       </div>
 
-      <div className="  bg-black/5">
-        <img src={Image3} alt="no internet" className="mt-5 lg:mt-10" />
+      <div className="bg-black/5 w-full lg:w-[40vw]">
+        <img src={Image3} alt="no internet" className="mt-5 lg:mt-10 w-full" />
       </div>
     </div>
   );
