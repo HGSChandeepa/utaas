d46c6055ba6c0f844c56b2f79e7affdf2bc5f8a2
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-03-11 11:15:46 +0530

    user data

--- frontend/src/Components/Sidebar/SidebarData.js
+++ frontend/src/Components/Sidebar/SidebarData.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { RxDashboard } from "react-icons/rx";
 import { FiActivity } from "react-icons/fi";
 import { GrTask } from "react-icons/gr";
@@ -6,41 +6,40 @@ import { GrInProgress } from "react-icons/gr";
 import { GrFavorite } from "react-icons/gr";
 import { CgProfile } from "react-icons/cg";
 export const SidebarData = [
-    {
-        title:'Dashboard',
-        path:'/dashboard',
-        icon:<RxDashboard />,
-        cName:'nav-text'
-        
-    },
-    {
-        title: 'MyActivity',
-        path: '/myactivity',
-        icon: <FiActivity />,
-        cName: 'nav-text'
-    },
-    {
-        title:'Tasks',
-        path:'/tasks',
-        icon:<GrTask />,
-        cName:'nav-text'
-    },
-    {
-        title:'Progress',
-        path:'/progress',
-        icon:<GrInProgress />,
-        cName:'nav-text'
-    },
-    {
-        title:'Favorites',
-        path:'/favorites',
-        icon:<GrFavorite />,
-        cName:'nav-text'
-    },
-    {
-        title:'Profile',
-        path:'/profile',
-        icon:<CgProfile />,
-        cName:'nav-text'
-    }
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: <RxDashboard />,
+    cName: "nav-text",
+  },
+  {
+    title: "Add New",
+    path: "/myactivity",
+    icon: <FiActivity />,
+    cName: "nav-text",
+  },
+  {
+    title: "Tasks",
+    path: "/tasks",
+    icon: <GrTask />,
+    cName: "nav-text",
+  },
+  {
+    title: "Progress",
+    path: "/progress",
+    icon: <GrInProgress />,
+    cName: "nav-text",
+  },
+  {
+    title: "Favorites",
+    path: "/favorites",
+    icon: <GrFavorite />,
+    cName: "nav-text",
+  },
+  {
+    title: "Profile",
+    path: "/profile",
+    icon: <CgProfile />,
+    cName: "nav-text",
+  },
 ];
