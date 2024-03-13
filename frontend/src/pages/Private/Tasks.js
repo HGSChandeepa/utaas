2001e0f3f82d31d4da246f9c6bb9c1734554cc06
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-03-13 13:08:12 +0530

    progress

--- frontend/src/pages/Private/Tasks.js
+++ frontend/src/pages/Private/Tasks.js
@@ -139,10 +139,10 @@ const Tasks = () => {
                       <div className="flex flex-row justify-between items-center">
                         <div className="flex gap-2">
                           <button className="bg-red-100 text-red-800 flex items-center gap-4   font-medium px-4 py-2 rounded-full text-md">
-                            Delete <FiDelete />
+                            Reject <FiDelete />
                           </button>
-                          <button className="bg-blue-100 text-blue-800 flex items-center gap-4   font-medium px-4 py-2 rounded-full text-md">
-                            Edit <TiEdit />
+                          <button className="bg-green-100 text-green-800 flex items-center gap-4   font-medium px-4 py-2 rounded-full text-md">
+                            Approve <TiEdit />
                           </button>
                         </div>
                       </div>
